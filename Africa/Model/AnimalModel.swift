d2c07a6154cd47fd0018ce65da2514799a815cd8
soroushamdg <soroushmpour@gmail.com>
2022-10-02 13:00:14 -0400

    Animals Model

---
+++ Africa/Model/AnimalModel.swift
@@ -0,0 +1,21 @@
+//
+//  AnimalModel.swift
+//  Africa
+//
+//  Created by Soro on 2022-10-02.
+//
+
+import Foundation
+
+struct Animal: Codable, Identifiable{
+    
+    let id: String
+    let name: String
+    let headline: String
+    let description: String
+    let link: String
+    let image: String
+    let gallery: [String]
+    let fact: [String]
+    
+}
