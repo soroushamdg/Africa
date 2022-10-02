57b4e782cbb117ebecb5d85b8cda0bb7a32b6513
soroushamdg <soroushmpour@gmail.com>
2022-10-02 12:44:29 -0400

    CoverImage implement.

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationView {
+            List{
+                CoverImageView()
+                    .frame(height: 300)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            }
+            .navigationBarTitle("Africa",displayMode: .large)
+            
+            
         }
-        .padding()
     }
 }
 
