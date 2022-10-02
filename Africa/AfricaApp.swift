3fbbc8975e28b75d842271d263ba040661789e09
soroushamdg <soroushmpour@gmail.com>
2022-10-02 11:27:20 -0400

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Soro on 2022-10-02.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
