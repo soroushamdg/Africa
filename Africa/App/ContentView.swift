80f69ec4260175cc6b50fb3ed7f964ddb7a1908e
soroushamdg <soroushmpour@gmail.com>
2022-10-06 22:21:39 -0400

    Complex animals list & grid view.

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -8,23 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let animals : [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    @State private var isGridViewActive: Bool = false
+    
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+
     var body: some View {
-        let animals : [Animal] = Bundle.main.decode("animals.json")
         NavigationView {
-            List{
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                
-                ForEach(animals) { animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                        AnimalListItemView(animal: animal)
+            Group {
+                if !isGridViewActive {
+                    List{
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            }
+                            
+                            
+                        }
                     }
+                } else {
+                    ScrollView(.vertical,showsIndicators: false){
+                        LazyVGrid(columns: gridLayout, alignment: .center,spacing: 10){
+                            ForEach(animals) { animal in
+                                NavigationLink(destination: AnimalDetailView(animal: animal)){
+                                    AnimalGridItemView(animal: animal)
+                                }
+                            }
+                        }
+                        .padding(10)
+                        .animation(.easeIn)
                         
-                        
+                    }
+                    
                 }
             }
             .navigationBarTitle("Africa",displayMode: .large)
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing){
+                    HStack(spacing: 16){
+                        Button(action: {
+                            print("list view is activated")
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                            
+                        }
+                        Button(action: {
+                            print("grid view is activated")
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                        }){
+                            Image(systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }
+                    }
+                }
+            }
+            
             
             
         }
