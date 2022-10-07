440d9b6c840d626bf39817ce8e57e72e7b8f0ddf
soroushamdg <soroushmpour@gmail.com>
2022-10-06 22:32:47 -0400

    Complex Grid layouts and switch between them.

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -14,7 +14,28 @@ struct ContentView: View {
     
     @State private var isGridViewActive: Bool = false
     
-    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+    @State private var gridLayout : [GridItem] = [ GridItem(.flexible()) ]
+    @State private var gridColumn: Int = 1
+    @State private var toolbarIcon: String = "square.grid.2x2"
+    
+    func gridSwitch(){
+        gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+        gridColumn = gridLayout.count
+        print("Grid Number : \(gridColumn)")
+        
+        switch gridColumn{
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x2"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2x2"
+        }
+
+
+    }
 
     var body: some View {
         NavigationView {
@@ -67,8 +88,9 @@ struct ContentView: View {
                             print("grid view is activated")
                             isGridViewActive = true
                             haptics.impactOccurred()
+                            gridSwitch()
                         }){
-                            Image(systemName: "square.grid.2x2")
+                            Image(systemName: toolbarIcon)
                                 .font(.title2)
                                 .foregroundColor(isGridViewActive ? .accentColor : .primary)
                         }
